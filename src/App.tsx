00a87206606f1ad48d3865fd545f99546f65fb66
Alex Vance <me@alexvance.io>
2021-11-18 19:54:43 -0700

    Functionality to flag cells

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,45 @@ import { generateCells } from "./lib/utils";
 import "./styles/App.css";
 
 function App() {
-  // TODO: will have a way to reset the game, so we'll need the setCells eventually
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [cellGrid, setCellGrid] = useState(() => generateCells());
+
+  const handleUpdateCell = (
+    row: number,
+    column: number,
+    isFlagged: boolean
+  ) => {
+    const updatedCell = { ...cellGrid[row][column], isFlagged };
+
+    const updatedRow = cellGrid[row].map((currentCell, columnNum) => {
+      if (columnNum === column) {
+        return updatedCell;
+      }
+      return currentCell;
+    });
+
+    const updatedGrid = cellGrid.map((currentRow, rowNum) => {
+      if (rowNum === row) {
+        return updatedRow;
+      }
+      return currentRow;
+    });
+
+    setCellGrid(updatedGrid);
+  };
 
   return (
     <div className="App">
       {cellGrid.map((rows, i) => {
         const rowOfCells = rows.map((cell, index) => (
-          <CellComponent key={index} isBomb={cell.isBomb} />
+          <CellComponent
+            key={index}
+            isBomb={cell.isBomb}
+            isFlagged={cell.isFlagged}
+            isHidden={cell.isHidden}
+            row={i}
+            column={index}
+            updateCell={handleUpdateCell}
+          />
         ));
         return (
           <div key={i} className="row">
--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -1,21 +1,32 @@
 import { SyntheticEvent } from "react";
 import "./styles.css";
 
-// TODO: will we want this instead?
-// type CellProps = Cell
-type CellProps = { isBomb: boolean };
+type CellProps = {
+  isBomb: boolean;
+  isFlagged: boolean;
+  isHidden: boolean;
+  row: number;
+  column: number;
+  updateCell: (row: number, column: number, isFlagged: boolean) => void;
+};
 
-// TODO: how to make the data so I can update it easily
-
-export default function CellComponent({ isBomb }: CellProps) {
+export default function CellComponent({
+  isBomb,
+  isFlagged,
+  isHidden,
+  row,
+  column,
+  updateCell,
+}: CellProps) {
   const handleRightClick = (e: SyntheticEvent) => {
     e.preventDefault();
     console.log("right click!");
+    updateCell(row, column, !isFlagged);
   };
 
   return (
     <div onContextMenu={(e) => handleRightClick(e)} className="cell">
-      {isBomb.toString()}
+      {isFlagged ? "flag" : ""}
     </div>
   );
 }
