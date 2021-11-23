e11560fdf7e16f3dc45762426c37501ad0a215ae
Alex Vance <me@alexvance.io>
2021-11-22 21:03:06 -0700

    WIP setup component

--- src/components/Setup/index.tsx
+++ src/components/Setup/index.tsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function Setup() {
-  return <div className="setup-wrapper">Setup</div>;
+  const [numberOfCells, setNumberOfCells] = useState("9");
+  const [numberOfBombs, setNumberOfBombs] = useState("3");
+
+  return (
+    <div className="setup-wrapper">
+      <div>
+        Number of Cells
+        <input
+          value={numberOfCells}
+          onChange={(e) => setNumberOfCells(e.target.value)}
+        />
+      </div>
+      <div>
+        Number of Bombs
+        <input
+          value={numberOfBombs}
+          onChange={(e) => setNumberOfBombs(e.target.value)}
+        />
+      </div>
+    </div>
+  );
 }
