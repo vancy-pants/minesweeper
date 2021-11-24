1551b96d8e4beccda0059664fdc067ce88b7b22b
Alex Vance <me@alexvance.io>
2021-11-24 15:27:04 -0700

    Set number of rows and columns

--- src/components/Setup/index.tsx
+++ src/components/Setup/index.tsx
@@ -2,20 +2,37 @@ import { useState } from "react";
 import "./styles.css";
 
 export default function Setup() {
-  const [numberOfCells, setNumberOfCells] = useState("9");
+  const [numberOfRows, setNumberOfRows] = useState("3");
+  const [numberOfColumns, setNumberOfColumns] = useState("3");
   const [numberOfBombs, setNumberOfBombs] = useState("3");
 
   return (
     <div className="setup-wrapper">
       <div>
-        Number of Cells
+        Number of Rows
+        <br />
         <input
-          value={numberOfCells}
-          onChange={(e) => setNumberOfCells(e.target.value)}
+          value={numberOfRows}
+          onChange={(e) => setNumberOfRows(e.target.value)}
         />
       </div>
+
+      <br />
+
+      <div>
+        Number of Columns
+        <br />
+        <input
+          value={numberOfColumns}
+          onChange={(e) => setNumberOfColumns(e.target.value)}
+        />
+      </div>
+
+      <br />
+
       <div>
         Number of Bombs
+        <br />
         <input
           value={numberOfBombs}
           onChange={(e) => setNumberOfBombs(e.target.value)}
